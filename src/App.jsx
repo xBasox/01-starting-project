2d108f8accfe96095e6c49b46dc1a9592ae5f7f0
xBasox <borko12329@gmail.com>
2025-03-01 17:12:25 +0200

    Fixed a bug where after clicking a button, the new state of the app would be a BLANK page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import { CORE_CONCEPTS } from "./data.js";
-import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
-import TabButton from "./components/TabButton.jsx";
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -13,7 +14,7 @@ function App() {
     setSelectedTopic(selectedButton);
   }
 
-  console.log("APP COMPONENT EXECUTING");
+  console.log('APP COMPONENT EXECUTING');
 
   let tabContent = <p>Please select a topic.</p>;
 
@@ -45,26 +46,26 @@ function App() {
           <h2>Examples</h2>
           <menu>
             <TabButton
-              isSelected={selectedTopic === "components"}
-              onSelect={() => handleSelect("components")}
+              isSelected={selectedTopic === 'components'}
+              onSelect={() => handleSelect('components')}
             >
               Components
             </TabButton>
             <TabButton
-              isSelected={selectedTopic === "jsx"}
-              onSelect={() => handleSelect("jsx")}
+              isSelected={selectedTopic === 'jsx'}
+              onSelect={() => handleSelect('jsx')}
             >
               JSX
             </TabButton>
             <TabButton
-              isSelected={selectedTopic === "props"}
-              onSelect={() => handleSelect("props")}
+              isSelected={selectedTopic === 'props'}
+              onSelect={() => handleSelect('props')}
             >
               Props
             </TabButton>
             <TabButton
-              isSelected={selectedTopic === "state"}
-              onSelect={() => handleSelect("state")}
+              isSelected={selectedTopic === 'state'}
+              onSelect={() => handleSelect('state')}
             >
               State
             </TabButton>
