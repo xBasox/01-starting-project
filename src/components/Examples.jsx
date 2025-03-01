32b767227f6a271b5456477edaf6c38a250511ae
xBasox <borko12329@gmail.com>
2025-03-01 20:36:44 +0200

    A bit of refactoring and deleting unneccesary lines of code.

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -25,7 +25,7 @@ export default function Examples() {
   return (
     <Section title={"Examples"} id="examples">
       <Tabs
-        buttonsContainer={"menu"}
+        ButtonsContainer={"menu"}
         buttons={
           <>
             <TabButton
