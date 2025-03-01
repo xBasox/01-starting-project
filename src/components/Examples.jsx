ed0df5d09d542ac1f06d40e3867cb8b4eaa369f7
xBasox <borko12329@gmail.com>
2025-03-01 20:29:23 +0200

    Setting button MENU type to be set dynamically, makes Tabs more reusable dynamic

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -29,6 +29,7 @@ export default function Examples() {
   return (
     <Section title={"Examples"} id="examples">
       <Tabs
+        buttonsContainer={"menu"}
         buttons={
           <>
             <TabButton
