a7e5f33d3f264f3ebe0eca32463a256454dd30ed
xBasox <borko12329@gmail.com>
2025-03-01 19:29:38 +0200

    Made Vite port forwarding set to fixed port since I kept getting numerious ports opened. Set static port for the local server

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: true,
+    port: 5175
+  },
   plugins: [react()]
 });
