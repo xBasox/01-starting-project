b66478cd09538b8861a2b8a63c38f57a4c1e0230
xBasox <borko12329@gmail.com>
2025-03-01 20:35:04 +0200

    Made the default state of the MENU to take one of the Menu items. When reloaded it will have one of the buttons clicked.

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -5,26 +5,22 @@ import Section from "./Section";
 import Tabs from "./Tabs";
 
 export default function Examples() {
-  const [selectedTopic, setSelectedTopic] = useState();
+  const [selectedTopic, setSelectedTopic] = useState("components");
 
   function handleSelect(selectedButton) {
     // selectedButton => 'components', 'jsx', 'props', 'state'
     setSelectedTopic(selectedButton);
   }
 
-  let tabContent = <p>Please select a topic.</p>;
-
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
-  }
+  const tabContent = (
+    <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </div>
+  );
 
   return (
     <Section title={"Examples"} id="examples">
