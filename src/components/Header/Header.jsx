86fc9f853240e76d02021d46d7c1474e77a88029
xBasox <borko12329@gmail.com>
2025-03-01 17:08:16 +0200

    Simple refactoring

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
-import reactImg from '../../assets/react-core-concepts.png';
-import './Header.css';
+import reactImg from "../../assets/react-core-concepts.png";
+import "./Header.css";
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,8 +1,8 @@
 export default function TabButton({ children, onSelect, isSelected }) {
-  console.log('TABBUTTON COMPONENT EXECUTING');
+  console.log("TABBUTTON COMPONENT EXECUTING");
   return (
     <li>
-      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
         {children}
       </button>
     </li>
